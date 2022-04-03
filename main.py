c50574f59e502e165a1a626c282f2fe443129834
ericparker14 <ericparker243@gmail.com>
2022-04-03 16:06:08 -0500

    some refactoring and organizing

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # TODO 1: print logo
-from datetime import date
 import art
 import random
 from game_data import data
+from os import system, name
 
 
-print(art.logo)
-SCORE = 0
+def clear():
+    if name == 'posix':
+        _ = system('clear')
+    else:
+        _ = system('clear')
+
+
 def compare():
     """Compares the two choices and evaluates players answer"""
     if choice_a['follower_count'] > choice_b['follower_count']:
@@ -21,20 +26,23 @@ def choice(player_choice):
         return 0
 
 
-#TODO 2 : pull 2 random choices from the game_data file and display their info
-
+print(art.logo)
+# get the first two choices out of the way
 choice_a, choice_b = random.choices(data, k = 2)
 
 
 def game():
-    game_is_playing = False
-    while game_is_playing:
+    clear()
+    SCORE = 0
+    is_game_over = False
+    while  not is_game_over:
+        global choice_a, choice_b
+        # to give the user info about choices
         print(f"Compare A: {choice_a['name']} a {choice_a['description'].lower()}, from {choice_a['country']}.")
         print(art.vs)
         print(f"Against B: {choice_b['name']} a {choice_b['description'].lower()}, from {choice_b['country']}.")
 
-        
-
+        # storing inputs to variables to use later
         answer = compare()
         player_answer = input("Who has more followers? Type 'A' or 'B':\n")
         player_choice = choice(player_choice=player_answer)
@@ -46,11 +54,12 @@ def game():
             print(f"Your score is {SCORE}!")
             choice_a = choice_b
             choice_b = random.choice(data)
+            # to fix duplicate bug
+            if choice_b == choice_a:
+                choice_b = random.choice(data)
         else:
             print("You are wrong. You lose")
-            game_is_playing = False
-
-
+            is_game_over = True
 
 
 play_game = input("Would you like to play the higher or lower game? Enter 'y' for yes and 'n' for no.")
